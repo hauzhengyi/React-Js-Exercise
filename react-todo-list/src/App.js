9899f3fc50e32df21301e08b565af728a0e69741
zhengyi <88762262+hauzhengyi@users.noreply.github.com>
2022-09-27 21:18:43 +0800

    todo list

---
+++ react-todo-list/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import React, { useState } from 'react'
+import Title from './components/Title'
+import ToDoList from './components/ToDoList'
+import NewToDo from './components/NewToDo'
+import todos from './data/mock-todos.json'
+
+function App() {
+
+  const [toDoList, setToDoList] = useState(todos);
+
+  const toggle = (id) => {
+    let newToDoList = toDoList.map(todo => {
+      return todo.id == id ? { ...todo, done: !todo.done } : { ...todo }
+    })
+    setToDoList(newToDoList)
+  }
+
+  return (
+    <div className="App">
+      <Title />
+      <ToDoList toDoList={toDoList} toggle={toggle} />
+      <NewToDo toDoList={toDoList} />
+    </div>
+  );
+}
+
+export default App;
